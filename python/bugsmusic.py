d8a0fded92f0bf0ca0d524315eb678728736b72f
박정민 <a01063363854@gmail.com>
2024-04-27 00:39:22 +0900

    2024

--- python/bugsmusic.py
+++ python/bugsmusic.py
@@ -14,6 +14,9 @@ soup = bs(res.text, "lxml")
 ranking = soup.select(".ranking > strong")
 title = soup.select(".title > a")
 artist = soup.select(".artist >a:nth-child(1)")
+image = soup.select(".trackList > tbody .thumbnail img ")
+
+# print(len(image))
 
 # print(len(ranking))
 # print(len(title))
@@ -41,13 +44,16 @@ artist = soup.select(".artist >a:nth-child(1)")
 rankingList = [r.text.strip() for r in ranking]
 titleList = [t.text.strip() for t in title]
 artistList = [a.text.strip() for a in artist]
+imgList = [img['src'].strip() for img in image]
 
 # 데이터 프레임 생성
 chart_df = pd.DataFrame({
     'Ranking' : rankingList,
     'Title': titleList,
-    'Artist' : artistList
+    'Artist' : artistList,
+    'Image' : imgList
 })
+
 
 # JSON 파일로 저장
 chart_df.to_json("bugsChart100.json", force_ascii=False, orient="records")
