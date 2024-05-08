6cadce61659b2e90418f565c31b0961eafb5717f
박정민 <a01063363854@gmail.com>
2024-05-08 16:25:47 +0900

    2024

--- chicken/kkanbu.py
+++ chicken/kkanbu.py
@@ -2,6 +2,7 @@ import requests as req
 from bs4 import BeautifulSoup as bs
 import json
 
+
 def get_menu_data(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'}
     res = req.get(url, headers=headers)
